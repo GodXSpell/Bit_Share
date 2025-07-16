563afdcbe19799124af0328c561e4a02fa6284c6
GodXSpell <tarunpreetsingh2696@gmail.com>
2025-07-17 04:41:08 +0530

    clean up

--- go.mod
+++
@@ -1,3 +0,0 @@
-module fileshare
-
-go 1.21
--- internal/utils/utils.go
+++
@@ -1,4 +0,0 @@
-package utils
-
-// Placeholder for utility functions used across the project.
-// We'll add logging, hashing, file metadata tools here later.
